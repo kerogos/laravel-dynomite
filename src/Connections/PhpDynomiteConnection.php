fd8d9b5d7dd37efbbd357e208d3f0367cc87c045
kerogos <kerog@wp.pl>
2021-08-23 16:56:52 +0200

    start repo

---
+++ src/Connections/PhpDynomiteConnection.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Kerogos\LaravelDynomite\Connections;
+
+
+use Illuminate\Redis\Connections\PhpRedisConnection;
+
+class PhpDynomiteConnection extends PhpRedisConnection
+{
+    public function flushdb()
+    {
+        $keys = $this->keys('*');
+        return $this->del($keys);
+    }
+    public function command($method, array $parameters = [])
+    {
+        if(config('dynomite.debug'))
+            \Log::debug('DynomiteConnectionCommand',['command' => $method]);
+        return parent::command($method, $parameters); 
+    }
+}
---
+++ src/DynomiteServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kerogos\LaravelDynomite;
+
+use Illuminate\Redis\RedisManager;
+use Illuminate\Redis\RedisServiceProvider;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\ServiceProvider;
+use Kerogos\LaravelDynomite\Connections\PhpDynomiteConnection;
+use function PHPUnit\Framework\never;
+
+class DynomiteServiceProvider extends RedisServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('redis', function ($app) {
+            $config = $app->make('config')->get('database.redis', []);
+            $redisManager = new RedisManager($app, Arr::pull($config, 'client', 'dynomite'), $config);
+            $redisManager->extend('dynomite',function (){
+                return new PhpDynomiteConnection;
+            });
+            return $redisManager;
+        });
+
+        $this->app->bind('redis.connection', function ($app) {
+            return $app['redis']->connection();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/dynomite.php' => config_path('dynomite.php')
+        ]);
+    }
+}
